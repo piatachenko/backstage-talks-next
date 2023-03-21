e70d69b12d129bce8ba4c3d909526e1dd84e4098
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-03-22 00:00:19 +0100

    responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import React, { useCallback, useEffect, useState } from "react";
 
 export default function App() {
   const [color, setColor] = useState("issue6");
-  const [link, setLink] = useState("");
+  const [element, setElement] = useState(
+    typeof window !== "undefined"
+      ? window.innerWidth >= 1024
+        ? "main"
+        : "div.main"
+      : "undefined"
+  );
 
   const innerHeight =
     typeof window !== "undefined" ? window.innerHeight : process.stdout.rows;
 
   const handleScroll = useCallback(() => {
-    const mainElement = document.querySelector("main");
+    const mainElement = document.querySelector(element);
     if (mainElement !== null) {
       const scrollPosition = mainElement.scrollTop;
       const issues = [
@@ -23,14 +28,27 @@ export default function App() {
         "issue1",
       ];
       const index = Math.round(scrollPosition / innerHeight);
-      const issue = issues[index];
+      const issue = issues[index] ? issues[index] : issues[issues.length - 1];
       setColor(issue);
-      setLink(issue);
     }
-  }, [innerHeight]);
+  }, [
+    innerHeight,
+    typeof window !== "undefined" ? window.innerWidth : process.stdout.rows,
+  ]);
+
+  const handleResize = () => {
+    if (typeof window !== "undefined") {
+      setElement(window.innerWidth >= 1024 ? "main" : "div.main");
+    }
+  };
 
   useEffect(() => {
-    const mainElement = document.querySelector("main");
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    const mainElement = document.querySelector(element);
     if (mainElement !== null) {
       mainElement.addEventListener("scroll", handleScroll);
       return () => {
@@ -139,7 +157,9 @@ export default function App() {
           rel="stylesheet"
         ></link>
       </Head>
-      <div className="relative text-lg font-semibold leading-[25px] text-[#251D20]">
+      <div
+        className={`main relative text-lg font-semibold leading-[25px] text-[#251D20] transition-all duration-[0.4s] ease-linear max-lg:overflow-y-scroll h-screen bg-${color}`}
+      >
         <div className="hidden">
           <div className="bg-issue6"></div>
           <div className="bg-issue5"></div>
@@ -148,28 +168,20 @@ export default function App() {
           <div className="bg-issue2"></div>
           <div className="bg-issue1"></div>
         </div>
-        <header className="absolute top-[1.25rem] inset-x-[0.9375rem]">
+        <header className="lg:absolute mt-[1.25rem] ml-[0.9375rem] max-lg:m b-[3.85rem]">
           <div className="flex items-stretch justify-between">
-            <div>
-              <img
-                src="https://backstagetalks.com/img/logo.png"
-                alt=""
-                width="260"
-              />
-            </div>
-            <div>
-              <Link href="mailto:info@backstagetalks.com">
-                info@backstagetalks.com
-              </Link>
-            </div>
+            <img
+              src="https://backstagetalks.com/img/logo.png"
+              alt=""
+              width="260"
+              className="max-md:w-[160px] max-md:mt-0.5 max-md:mb-[0.575rem]"
+            />
           </div>
         </header>
-        <main
-          className={`snap-y scroll-smooth snap-mandatory w-screen h-screen overflow-scroll transition-all duration-700 ease-linear bg-${color}`}
-        >
+        <main className="lg:snap-y scroll-smooth lg:snap-mandatory lg:w-screen lg:h-screen lg:overflow-scroll">
           <section
             id="issue6"
-            className="flex items-center justify-center snap-always snap-center w-screen h-screen"
+            className="flex items-center justify-center snap-always snap-center lg:w-screen h-screen text-center max-lg:pb-[3.95rem] max-lg:-mb-[2rem]"
           >
             <article className="flex flex-col items-center w-full my-auto">
               <img
@@ -202,7 +214,7 @@ export default function App() {
           </section>
           <section
             id="issue5"
-            className="flex items-center justify-center snap-always snap-center w-screen h-screen"
+            className="flex items-center justify-center snap-always snap-center lg:w-screen h-screen text-center"
           >
             <article className="flex flex-col items-center w-full my-auto">
               <img
@@ -235,7 +247,7 @@ export default function App() {
           </section>
           <section
             id="issue4"
-            className="flex items-center justify-center snap-always snap-center w-screen h-screen"
+            className="flex items-center justify-center snap-always snap-center lg:w-screen h-screen text-center"
           >
             <article className="flex flex-col items-center w-full my-auto">
               <img
@@ -244,7 +256,7 @@ export default function App() {
                 className="w-[80%] max-w-[26.25rem]"
               ></img>
               <div>Issue #4 is sold out.</div>
-              <div className="text-[0.9375rem] mt-5 mb-[0.8rem]">
+              <div className="text-[0.9375rem] mt-5 mb-[0.8rem] mx-1">
                 If you are lucky, you may get the last pieces in{" "}
                 <a
                   href="http://backstagetalks.com/stocklist.php"
@@ -258,7 +270,7 @@ export default function App() {
           </section>
           <section
             id="issue3"
-            className="flex items-center justify-center snap-always snap-center w-screen h-screen"
+            className="flex items-center justify-center snap-always snap-center lg:w-screen h-screen text-center"
           >
             <article className="flex flex-col items-center w-full my-auto">
               <img
@@ -293,7 +305,7 @@ export default function App() {
           </section>
           <section
             id="issue2"
-            className="flex items-center justify-center snap-always snap-center w-screen h-screen"
+            className="flex items-center justify-center snap-always snap-center lg:w-screen h-screen text-center"
           >
             <article className="flex flex-col items-center w-full my-auto">
               <img
@@ -326,7 +338,7 @@ export default function App() {
           </section>
           <section
             id="issue1"
-            className="flex items-center justify-center snap-always snap-center w-screen h-screen"
+            className="flex items-center justify-center snap-always snap-center lg:w-screen h-screen text-center"
           >
             <article className="flex flex-col items-center w-full my-auto">
               <img
@@ -335,7 +347,7 @@ export default function App() {
                 className="w-[80%] max-w-[26.25rem]"
               ></img>
               <div>Issue #1 is sold out.</div>
-              <div className="text-[0.9375rem] mt-5 mb-[0.8rem]">
+              <div className="text-[0.9375rem] mt-5 mb-[0.8rem] mx-1">
                 If you are lucky, you may get the last pieces in{" "}
                 <a
                   href="http://backstagetalks.com/stocklist.php"
@@ -348,58 +360,75 @@ export default function App() {
             </article>
           </section>
         </main>
-        <aside className="absolute bottom-5 right-[0.95rem] flex flex-col">
+        <aside className="absolute hidden bottom-5 right-[0.95rem] lg:flex flex-col">
           <Link
             href="#issue6"
-            className={`mt-[0.23rem] ${link === "issue6" ? "" : "font-normal"}`}
+            className={`mt-[0.23rem] ${
+              color === "issue6" ? "" : "font-normal"
+            }`}
           >
             Issue #6
           </Link>
           <Link
             href="#issue5"
-            className={`mt-[0.23rem] ${link === "issue5" ? "" : "font-normal"}`}
+            className={`mt-[0.23rem] ${
+              color === "issue5" ? "" : "font-normal"
+            }`}
           >
             Issue #5
           </Link>
           <Link
             href="#issue4"
-            className={`mt-[0.23rem] ${link === "issue4" ? "" : "font-normal"}`}
+            className={`mt-[0.23rem] ${
+              color === "issue4" ? "" : "font-normal"
+            }`}
           >
             Issue #4
           </Link>
           <Link
             href="#issue3"
-            className={`mt-[0.23rem] ${link === "issue3" ? "" : "font-normal"}`}
+            className={`mt-[0.23rem] ${
+              color === "issue3" ? "" : "font-normal"
+            }`}
           >
             Issue #3
           </Link>
           <Link
             href="#issue2"
-            className={`mt-[0.26rem] ${link === "issue2" ? "" : "font-normal"}`}
+            className={`mt-[0.26rem] ${
+              color === "issue2" ? "" : "font-normal"
+            }`}
           >
             Issue #2
           </Link>
           <Link
             href="#issue1"
-            className={`mt-[0.2rem] ${link === "issue1" ? "" : "font-normal"}`}
+            className={`mt-[0.2rem] ${color === "issue1" ? "" : "font-normal"}`}
           >
             Issue #1
           </Link>
         </aside>
-        <footer className="absolute left-[1.375rem] bottom-[1.1rem] w-80">
-          <p className="mb-7">
-            Backstage Talks is a&nbsp;magazine of&nbsp;casual, but in&nbsp;depth
-            dialogues on&nbsp;design and business. Our decisions shape and
-            influence this complex world—to have a&nbsp;chance to&nbsp;make the
-            right ones, we need to talk.
-          </p>
-          <div>
-            <a
-              href="https://backstagetalks.com/privacy-policy.php"
-              className="underline"
-            >
-              Privacy Policy
-            </a>
+        <footer className="max-lg:flex max-md:flex-col max-md:items-center max-md:text-center items-end justify-between">
+          <div className="lg:absolute left-[1.375rem] md:max-lg:ml-[0.95rem] bottom-[1.1rem] md:max-lg:mb-[1.1rem] md:w-80 max-md:mx-4">
+            <p className="mb-9 md:mb-7">
+              Backstage Talks is a&nbsp;magazine of&nbsp;casual, but
+              in&nbsp;depth dialogues on&nbsp;design and business. Our decisions
+              shape and influence this complex world—to have a&nbsp;chance
+              to&nbsp;make the right ones, we need to talk.
+            </p>
+            <div>
+              <a
+                href="https://backstagetalks.com/privacy-policy.php"
+                className="underline"
+              >
+                Privacy Policy
+              </a>
+            </div>
+          </div>
+          <div className="lg:absolute top-[1.25rem] md:max-lg:mb-[1.1rem] right-[0.9375rem] md:max-lg:mx-[0.95rem] max-md:mb-[3.15rem] max-md:mt-[3.1rem]">
+            <Link href="mailto:info@backstagetalks.com">
+              info@backstagetalks.com
+            </Link>
           </div>
         </footer>
       </div>
